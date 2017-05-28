ca2e42f724647f88d51564bd7117aa32b68d4d42
Samuel Oey <Samuel Oey>
2017-05-28 18:48:45 +0700

    initial commit

---
+++ application/views/include/js.php
@@ -0,0 +1,24 @@
+<!-- Javascript Files
+    ================================================== -->
+
+    <!-- initialize jQuery Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Preloader js file -->
+    <script src="<?php echo base_url(); ?>assets/landing/js/queryloader2.min.js" type="text/javascript"></script>
+    <!-- For smooth animatin  -->
+    <script src="<?php echo base_url(); ?>assets/landing/js/wow.min.js"></script>  
+    <!-- Bootstrap js -->
+    <script src="<?php echo base_url(); ?>assets/landing/js/bootstrap.min.js"></script>
+    <!-- slick slider -->
+    <script src="<?php echo base_url(); ?>assets/landing/js/slick.min.js"></script>
+    <!-- superslides slider -->
+    <script src="<?php echo base_url(); ?>assets/landing/js/jquery.easing.1.3.js"></script>
+    <script src="<?php echo base_url(); ?>assets/landing/js/jquery.animate-enhanced.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/landing/js/jquery.superslides.min.js" type="text/javascript" charset="utf-8"></script>   
+    <!-- for circle counter -->
+    <script src='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/js/jquery.circliful.min.js'></script>
+    <!-- Gallery slider -->
+    <script type="text/javascript" language="javascript" src="<?php echo base_url(); ?>assets/landing/js/jquery.tosrus.min.all.js"></script>   
+   
+    <!-- Custom js-->
+    <script src="<?php echo base_url(); ?>assets/landing/js/custom.js"></script>
---
+++ application/views/keunggulan.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--=============================================== 
+    Template Design By WpFreeware Team.
+    Author URI : http://www.wpfreeware.com/
+    ====================================================-->
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>Galileo Gasing: Keunggulan</title>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/icon" href="img/wpf-favicon.png"/>
+
+	<!-- css -->
+		<?php include("include/css.php"); ?>
+   
+  </head>
+  <body>    
+
+    <!-- SCROLL TOP BUTTON -->
+    <a class="scrollToTop" href="#"></a>
+    <!-- END SCROLL TOP BUTTON -->
+
+    <!--=========== BEGIN HEADER SECTION ================-->
+		<?php include("include/header.php"); ?>
+    <!--=========== END HEADER SECTION ================--> 
+
+	<section id="imgBanner">
+      <h2>Keunggulan</h2>
+    </section>
+    <!--=========== BEGIN WHY US SECTION ================-->
+    <br><br><section id="whyUs">
+      <!-- Start why us top -->
+      <div class="row">        
+        <div class="col-lg-12 col-sm-12">
+          <div class="whyus_top">
+            <div class="container">
+              <!-- Why us top titile -->
+              <div class="row">
+                <div class="col-lg-12 col-md-12"> 
+                  <div class="title_area">
+                    <h2 class="title_two">Keunggulan kami</h2>
+                    <span></span> 
+                  </div>
+                </div>
+              </div>
+              <!-- End Why us top titile -->
+              <!-- Start Why us top content  -->
+              <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                  <div class="single_whyus_top wow fadeInUp">
+                    <div class="whyus_icon">
+                      <span class="fa fa-desktop"></span>
+                    </div>
+                    <h3>Technology</h3>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                  <div class="single_whyus_top wow fadeInUp">
+                    <div class="whyus_icon">
+                      <span class="fa fa-users"></span>
+                    </div>
+                    <h3>Best Tutor</h3>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                  <div class="single_whyus_top wow fadeInUp">
+                    <div class="whyus_icon">
+                      <span class="fa fa-flask"></span>
+                    </div>
+                    <h3>Practical Training</h3>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                  <div class="single_whyus_top wow fadeInUp">
+                    <div class="whyus_icon">
+                      <span class="fa fa-support"></span>
+                    </div>
+                    <h3>Support</h3>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                  </div>
+                </div>
+              </div>
+              <!-- End Why us top content  -->
+            </div>
+          </div>
+        </div>        
+      </div>
+      <!-- End why us top -->
+
+    </section>
+    <!--=========== END WHY US SECTION ================-->
+
+    <!--=========== BEGIN OUR COURSES SECTION ================-->
+    <section id="ourCourses">
+      <div class="container">
+       <!-- Our courses titile -->
+        <div class="row">
+          <div class="col-lg-12 col-md-12"> 
+            <div class="title_area">
+              <h2 class="title_two">Our Courses</h2>
+              <span></span> 
+            </div>
+          </div>
+        </div>
+        <!-- End Our courses titile -->
+        <!-- Start Our courses content -->
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="ourCourse_content">
+              <ul class="course_nav">
+                <li>
+                  <div class="single_course">
+                    <div class="singCourse_imgarea">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/course-1.jpg" />
+                      <div class="mask">                         
+                        <a href="#" class="course_more">View Course</a>
+                      </div>
+                    </div>
+                    <div class="singCourse_content">
+                    <h3 class="singCourse_title"><a href="#">Introduction To Matrix</a></h3>
+                    <p class="singCourse_price"><span>$20</span> Per One Month</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                    <div class="singCourse_author">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/author.jpg" alt="img">
+                      <p>Richard Remus, Teacher</p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="single_course">
+                    <div class="singCourse_imgarea">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/course-2.jpg" />
+                      <div class="mask">                         
+                        <a href="#" class="course_more">View Course</a>
+                      </div>
+                    </div>
+                    <div class="singCourse_content">
+                    <h3 class="singCourse_title"><a href="#">Introduction To Matrix</a></h3>
+                    <p class="singCourse_price"><span>$20</span> Per One Month</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                    <div class="singCourse_author">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/author.jpg" alt="img">
+                      <p>Richard Remus, Teacher</p>
+                    </div>
+                  </div>
+                </li> 
+                <li>
+                  <div class="single_course">
+                    <div class="singCourse_imgarea">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/course-1.jpg" />
+                      <div class="mask">                         
+                        <a href="#" class="course_more">View Course</a>
+                      </div>
+                    </div>
+                    <div class="singCourse_content">
+                    <h3 class="singCourse_title"><a href="#">Introduction To Matrix</a></h3>
+                    <p class="singCourse_price"><span>$20</span> Per One Month</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                    <div class="singCourse_author">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/author.jpg" alt="img">
+                      <p>Richard Remus, Teacher</p>
+                    </div>
+                  </div>
+                </li>  
+                <li>
+                  <div class="single_course">
+                    <div class="singCourse_imgarea">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/course-2.jpg" />
+                      <div class="mask">                         
+                        <a href="#" class="course_more">View Course</a>
+                      </div>
+                    </div>
+                    <div class="singCourse_content">
+                    <h3 class="singCourse_title"><a href="#">Introduction To Matrix</a></h3>
+                    <p class="singCourse_price"><span>$20</span> Per One Month</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                    <div class="singCourse_author">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/author.jpg" alt="img">
+                      <p>Richard Remus, Teacher</p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="single_course">
+                    <div class="singCourse_imgarea">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/course-1.jpg" />
+                      <div class="mask">                         
+                        <a href="#" class="course_more">View Course</a>
+                      </div>
+                    </div>
+                    <div class="singCourse_content">
+                    <h3 class="singCourse_title"><a href="#">Introduction To Matrix</a></h3>
+                    <p class="singCourse_price"><span>$20</span> Per One Month</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                    <div class="singCourse_author">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/author.jpg" alt="img">
+                      <p>Richard Remus, Teacher</p>
+                    </div>
+                  </div>
+                </li> 
+                <li>
+                  <div class="single_course">
+                    <div class="singCourse_imgarea">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/course-2.jpg" />
+                      <div class="mask">                         
+                        <a href="#" class="course_more">View Course</a>
+                      </div>
+                    </div>
+                    <div class="singCourse_content">
+                    <h3 class="singCourse_title"><a href="#">Introduction To Matrix</a></h3>
+                    <p class="singCourse_price"><span>$20</span> Per One Month</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                    <div class="singCourse_author">
+                      <img src="<?php echo base_url(); ?>assets/landing/img/author.jpg" alt="img">
+                      <p>Richard Remus, Teacher</p>
+                    </div>
+                  </div>
+                </li>                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- End Our courses content -->
+      </div>
+    </section>
+    <!--=========== END OUR COURSES SECTION ================-->  
+
+    
+    <!--=========== BEGIN FOOTER SECTION ================-->
+		<?php include("include/footer.php"); ?>
+    <!--=========== END FOOTER SECTION ================--> 
+
+  
+
+    <!--javascript-->
+		<?php include("include/js.php"); ?>
+	
+    <!--=============================================== 
+    Template Design By WpFreeware Team.
+    Author URI : http://www.wpfreeware.com/
+    ====================================================-->
+
+
+  </body>
+</html>
