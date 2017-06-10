68114f5e719a868c4548debf120d442b6455e23a
Samuel Oey <Samuel Oey>
2017-06-10 23:38:13 +0700

    student registration

---
+++ include/sidebar.php
@@ -0,0 +1,48 @@
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+              
+              	  <p class="centered"><a href="profile.html"><img src="assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+              	  <h5 class="centered">Marcel Newman</h5>
+              	  	
+                  <li class="mt">
+                      <a class="active" href="index.php">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Beranda</span>
+                      </a>
+                  </li>
+				  <li class="mt" style="margin-top:8px">
+                      <a href="profil.php">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Profil</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-desktop"></i>
+                          <span>Pembelajaran</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="konsltasi.php">Konsultasi</a></li>
+                          <li><a  href="modul.php">Download Modul</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-cogs"></i>
+                          <span>Lihat</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="calendar.html">Calendar Bimbel</a></li>
+                          <li><a  href="jadwal_bimbel.php">Jadwal Bimbel</a></li>
+                          <li><a  href="jadwal_tutor.php">Jadwal Tutor</a></li>
+                      </ul>
+                  </li>
+                  
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
---
+++ profil.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Elearning Galileo | Beranda</title>
+
+    <?php include("include/css.php");?>
+	
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+  <section id="container" >
+      <!-- **********************************************************************************************************************************************************
+      TOP BAR CONTENT & NOTIFICATIONS
+      *********************************************************************************************************************************************************** -->
+      <!--header start-->
+		<?php include("include/header.php");?>
+      <!--header end-->
+      
+      <!-- **********************************************************************************************************************************************************
+      MAIN SIDEBAR MENU
+      *********************************************************************************************************************************************************** -->
+      <!--sidebar start-->
+		<?php include("include/sidebar.php"); ?>
+      <!--sidebar end-->
+      
+      <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+
+              <div class="row">
+                  <div class="col-lg-9 main-chart">      
+						<h2>Profile, Marcel Newman</h2>				  
+                  </div><!-- /col-lg-9 END SECTION MIDDLE -->
+                  
+                  
+      <!-- **********************************************************************************************************************************************************
+      RIGHT SIDEBAR CONTENT
+      *********************************************************************************************************************************************************** -->                  
+                  
+                 
+              </div><! --/row -->
+          </section>
+      </section>
+
+      <!--main content end-->
+      <!--footer start-->
+      <?php include("include/footer.php"); ?>
+      <!--footer end-->
+  </section>
+
+	 <!--javascript start-->
+		<?php include("include/js.php"); ?>
+	 <!--javascript end-->
+	 
+  
+
+  </body>
+</html>
